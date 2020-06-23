93cc99a9c49a365b65b37eda410cf29be2cde3bb
Mina <55865233+i333006@users.noreply.github.com>
2020-06-23 18:32:54 +0800

    rename based on naming conversion.

--- src/main/java/org/akhq/modules/KafkaModule.java
+++ src/main/java/org/akhq/modules/KafkaModule.java
@@ -56,7 +56,6 @@ public class KafkaModule {
 
     private String caCert = CFEnvUtils.getRootCertUrl("");
 
-    private String bootstrapServers = CFEnvUtils.getBootstrapServers_AuthSSL("");
     private String bootstrapServers = CFEnvUtils.getBootstrapServersAuthSSL("");
 
     private String trustStorePass = String.valueOf(UUID.randomUUID());
