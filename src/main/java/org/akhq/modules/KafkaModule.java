e6cdbd320843bc7b597af6df54f45140b8520a32
Mina <55865233+i333006@users.noreply.github.com>
2020-06-23 17:59:10 +0800

    keep import package style.

--- src/main/java/org/akhq/modules/KafkaModule.java
+++ src/main/java/org/akhq/modules/KafkaModule.java
@@ -32,7 +32,11 @@ import java.io.IOException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Singleton
